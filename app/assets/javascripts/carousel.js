0b8d1ef7569b6175868aa74547d94dd8b0e12044
Troy Starwalt <troy.starwalt@gmail.com>
2016-03-12 11:21:11 -0500

    testing

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,14 +1,14 @@
 $(document).on("ready page:load", function(){
-  $('.carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    centerMode: true,
-    variableWidth: true,
-    variableHeight: true,
-    arrows: true,
-    cssEase: 'linear'
-  });
+    console.log("hi");
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        centerMode: true,
+        variableWidth: true,
+        variableHeight: true,
+        arrows: true,
+        cssEase: 'linear'
+    });
 });
 
-console.log("hi");
