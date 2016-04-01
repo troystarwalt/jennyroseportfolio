aeb4a6e8f9dad2f08404075f55b8e26695a4bed9
Troy Starwalt <troy.starwalt@gmail.com>
2016-04-01 14:53:23 -0400

    precompiled

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,12 +1,14 @@
 $(document).on("ready page:load", function(){
     console.log("hi");
     $('.carousel').slick({
-        dots: true,
         infinite: true,
         speed: 300,
         centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
         variableWidth: true,
         variableHeight: true,
+        adaptiveHeight: true,
         arrows: true,
         cssEase: 'linear'
     });
