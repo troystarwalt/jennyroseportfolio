652bd6ee859430bebdbcfad9c1fa68d51e44b00e
Troy Starwalt <troy.starwalt@gmail.com>
2016-10-13 21:08:51 -0400

    very basic surprise

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -21,9 +21,17 @@ class MainController < ApplicationController
      UserMailer.new_message(@message).deliver
      redirect_to(root_path, :notice => "Thanks for reaching out. I'll get back to you soon!")
 
-      else 
+      else
     redirect_to(contact_path, :notice => "Sorry you need to include a real email.")
 
     end
   end
+
+	def surprise
+		@due_date = Date.parse('24-04-2017')
+		@pretty_date = @due_date.strftime("%A, %b %d, %Y")
+		current_date = Date.today
+		@days_left = (@due_date - current_date).to_i
+		render :layout => false
+	end
 end
